f6ba8f5d50c3058a5f15a2351c30add303dcb491
Sam <siacavazzi@gmail.com>
2023-06-16 18:02:09 -0400

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, occasion) {
+    let output = [];
+    for (let i = 0; i < names.length; i++) {
+        output.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+    }
+    return output;
+}
+
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n);
+        --n;
+    }
+}
